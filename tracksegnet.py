709bc9196673cf8d87f038e47cb6e6122f1b64bc
hkabbech <helene.kabbech@gmail.com>
2023-03-13 20:58:11 +0100

    update main function and parms file

--- tracksegnet.py
+++ tracksegnet.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 """
-    Segmentation of trajectories into N tracklet states using LSTM neural network classifier
+    Segmentation of trajectories into N diffusive states using LSTM neural network classifier
 
     Usage:
-        # Change the parameters in the main before running the following command:
-        ./dl_mss.py
+        # Adjust parameters in parms.csv before running the following command on the terminal:
+        python tracksegnet.py parms.csv
 """
 
 # Third-party modules
@@ -20,17 +20,10 @@ from tensorflow.keras.models import load_model
 
 # Local modules
 from src.simulate_tracks import run_track_simulation
-from src.experimental_tracks import extract_all_mdf, predict_states
+from src.experimental_tracks import extract_all_tracks, predict_states
 from src.generate_lstm_model import generate_lstm_model
 from src.analysis import compute_ptm, make_tracklet_lists, compute_all_msd,\
-    plot_scatter_alpha_diffusion, plot_proportion
-
-## For GPU usage, uncomment the following lines:
-# from tensorflow.compat.v1 import ConfigProto
-# from tensorflow.compat.v1 import InteractiveSession
-# CONFIG = ConfigProto()
-# CONFIG.gpu_options.allow_growth = True
-# SESSION = InteractiveSession(config=CONFIG)
+    plot_scatter_alpha_diffusion
 
 
 def get_color_list(num_states):
@@ -55,14 +48,26 @@ if __name__ == "__main__":
 
     START_TIME = datetime.now()
 
-    PARMS_FILENAME = 'parms.csv'
     # PARMS_FILENAME = sys.argv[0]
+    PARMS_FILENAME = 'parms.csv'
     PARMS_DF = pd.read_csv(PARMS_FILENAME, sep='\t').set_index('parms').squeeze().to_dict()
 
     NSTATES = int(PARMS_DF['num_states'])
+
+    # test PARMS_DF
+    for state in range(1, NSTATES+1):
+        if not f'state_{state}_diff' in PARMS_DF:
+            raise ValueError(f'state_{state}_diff not defined in parms.csv')
+        if not f'state_{state}_alpha' in PARMS_DF:
+            raise ValueError(f'state_{state}_alpha not defined in parms.csv')
+        for state2 in range(1, NSTATES+1):
+            if not f'pt_{state}_{state2}' in PARMS_DF:
+                raise ValueError(f'pt_{state}_{state2} not defined in parms.csv')
+       
     PARMS = {
         ## Path to the trajectories to analyze:
         'data_path': Path(PARMS_DF['data_path']),
+        'track_format': 'MDF',
 
         ## Microscope settings:
         'time_frame': float(PARMS_DF['time_frame']), # Time between two frames in second
@@ -88,7 +93,7 @@ if __name__ == "__main__":
         'track_length': 27,
         'num_simulate_tracks': 10000,
         # transition probabilities
-        'ptm': np.zeros(NSTATES) + 0.1 + (1 - NSTATES / 10) * np.identity(NSTATES),
+        'ptm': np.array([[float(PARMS_DF[f'pt_{state1}_{state2}']) for state1 in range(1, NSTATES+1)] for state2 in range(1, NSTATES+1)]),
         'min_frames':4, # Minimal amount of frames
         'min_frames_in_same_state':4, # Minimal amount of frames for particles remaining in a state
         'beta':100, # Mean of exponential distribution for length tracks
@@ -150,7 +155,7 @@ if __name__ == "__main__":
 
     TRACK_CSV = PARMS['result_path']/f"{PARMS['data_path'].name}_tracks_df.csv"
     if not os.path.isfile(TRACK_CSV):
-        TRACK_DF = extract_all_mdf(PARMS)
+        TRACK_DF = extract_all_tracks(PARMS)
     else:
         print('\nLoad experimental trajectories...')
         TRACK_DF = pd.read_csv(TRACK_CSV)
@@ -160,9 +165,17 @@ if __name__ == "__main__":
 
     ## ANALYSIS
     ###########
-    print(compute_ptm(TRACK_DF, PARMS))
-    TRACKLET_LISTS = make_tracklet_lists(TRACK_DF, PARMS)
-    MOTION_PARMS = compute_all_msd(TRACKLET_LISTS, PARMS)
+    PTM_CSV = PARMS['result_path']/'transition_probabilities.csv'
+    if not os.path.isfile(PTM_CSV):
+        PTM = compute_ptm(TRACK_DF, PARMS)
+    else:
+        PTM = pd.read_csv(PTM_CSV)
+    print(PTM)
+
+    if not os.path.isfile(PARMS['result_path']/"motion_parms_state_1.csv"):
+        TRACKLET_LISTS = make_tracklet_lists(TRACK_DF, PARMS)
+        MOTION_PARMS = compute_all_msd(TRACKLET_LISTS, PARMS)
+    else:
+        MOTION_PARMS = [pd.read_csv(PARMS['result_path']/f"motion_parms_state_{state+1}.csv") for state in range(PARMS['num_states'])]
+
     plot_scatter_alpha_diffusion(MOTION_PARMS, PARMS)
-    # TODO in scatterplot: markersize and distribution based on the number of data points!
-    plot_proportion(TRACKLET_LISTS, PARMS)
